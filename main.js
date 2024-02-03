31a73db94a10baca4ef066b784ef7afface8202f
LoganWarren <law234@pitt.edu>
2024-02-02 23:13:39 -0500

    map

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+function onMapClick(e) {
+    var marker = L.marker(e.latlng).addTo(map);
+    marker.bindPopup("Your tagged location").openPopup();
+}
+map.on('click', onMapClick);
